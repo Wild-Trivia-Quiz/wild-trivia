5f22df54fa1fd1232ce6239f0c65cf91cfd327fb
ManuelCaramelo <manuel.caramelo@gmail.com>
2020-04-17 13:03:40 +0100

    nested routing in game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,55 @@
 import React from 'react';
+import {
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
 
-  render() {
-    return <div>...</div>;
-  }
+
+function Game() {
+ 
+    const match = useRouteMatch();
+    return (
+      <div>
+        <h2>Topics</h2>
+  
+        <ul>
+          <li>
+            <Link to={`${match.url}/triviaA`}>A</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/triviaB`}>
+              B
+            </Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/triviaC`}>
+              C
+            </Link>
+          </li>
+        </ul>
+  
+        <Switch>
+          <Route path={`${match.path}/:gameId`}>
+            <ChoosenGame />
+          </Route>
+          <Route path={match.path}>
+            <h3>Please select a Game.</h3>
+          </Route>
+        </Switch>
+      </div>
+    );
+  
 }
 
+
 export default Game;
+
+
+function ChoosenGame() {
+  const { gameId } = useParams();
+  return <h3>Requested game ID: {gameId}</h3>;
+}
