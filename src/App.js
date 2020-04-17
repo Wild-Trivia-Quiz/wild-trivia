84a3731b97501be486ef178f0592d89e135c89b6
amduraes <andremdpereira@gmail.com>
2020-04-17 11:01:20 +0100

    created-homepage-detailpage-about-components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <div className='spinner-border' role='status'>
-        <span className='sr-only'>Loading...</span>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Homepage</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/detailpage'>Detail Page</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path='/'>
+            <Homepage />
+          </Route>
+          <Route path='/detailpage'>
+            <DetailPage />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+        </Switch>
       </div>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    </Router>
   );
+}
+
+function Homepage() {
+  return <h2>Homepage</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function DetailPage() {
+  return <h2>Detail Page</h2>;
 }
 
 export default App;
