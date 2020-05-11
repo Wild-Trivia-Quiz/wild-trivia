bd9551794d9437d7a4c5061fcf96a39cf2d40c92
amduraes <andremdpereira@gmail.com>
2020-05-11 19:20:34 +0100

    state-leak-workaround

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,11 @@
-import React, { useState, useContext } from "react";
-import GameIntro from "./GameIntro";
-import GameUI from "./GameUI";
-import { QuizAPIContext } from "../contexts/QuizAPIContext";
+import React, { useState, useContext } from 'react';
+import GameIntro from './GameIntro';
+import GameUI from './GameUI';
+import { QuizAPIContext } from '../contexts/QuizAPIContext';
 
 const Game = () => {
   const { quizData } = useContext(QuizAPIContext);
-  console.log(quizData && quizData);
+  console.log(quizData);
   const [isGameUILoaded, setIsGameUILoaded] = useState(false);
 
   const handleClick = () => {
