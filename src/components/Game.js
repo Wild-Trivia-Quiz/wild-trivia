96a28b928925e0a00bea00faee4b164ab07a6513
amduraes <andremdpereira@gmail.com>
2020-05-13 12:34:04 +0100

    created new context and round component Co-authored-by: Filipa Marta

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,37 +1,32 @@
-import React, { useContext, useEffect } from "react";
-import PropTypes from "prop-types";
-import { Container, Row, Col } from "react-bootstrap";
-import BackBtn from "./BackBtn";
-import QuestionAnswers from "./QuestionAnswers";
-import { QuizAPIContext } from "../contexts/QuizAPIContext";
-import "./Game.scss";
+import React, { useContext, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import BackBtn from './BackBtn';
+import QuestionAnswers from './QuestionAnswers';
+import { QuizAPIContext } from '../contexts/QuizAPIContext';
+import './Game.scss';
 
 const Game = () => {
   const {
     toggleShouldRedirectGame,
-    quizData,
     quizName,
     quizCategoryName,
-    quizQuestions,
     quizInstructions,
   } = useContext(QuizAPIContext);
-  //console.log(quizData);
 
   useEffect(() => {
     toggleShouldRedirectGame();
   }, []);
 
   return (
-    <section className="game">
-      <BackBtn url="/" />
+    <section className='game'>
+      <BackBtn url='/' />
       <Container fluid>
-        <Row className="content-block">
+        <Row className='content-block'>
           <Col
             xs={12}
             sm={12}
             md={{ span: 8, offset: 2 }}
-            lg={{ span: 8, offset: 2 }}
-          >
+            lg={{ span: 8, offset: 2 }}>
             <h1>{quizName}</h1>
             <p>Category: {quizCategoryName}</p>
           </Col>
@@ -39,31 +34,28 @@ const Game = () => {
             xs={12}
             sm={12}
             md={{ span: 8, offset: 2 }}
-            lg={{ span: 8, offset: 2 }}
-          >
+            lg={{ span: 8, offset: 2 }}>
             <p>{quizInstructions}</p>
           </Col>
         </Row>
-        <Row className="content-block">
+        <Row className='content-block'>
           <Col
             xs={12}
             sm={12}
             md={{ span: 8, offset: 2 }}
-            lg={{ span: 8, offset: 2 }}
-          >
-            <QuestionAnswers />;
-          </Col>
-        </Row>
-        <Row className="content-block">
-          <Col
-            xs={12}
-            sm={12}
-            md={{ span: 8, offset: 2 }}
-            lg={{ span: 8, offset: 2 }}
-          >
-            <button type="submit" className="arcade-btn">
+            lg={{ span: 8, offset: 2 }}>
+            <button type='submit' className='arcade-btn'>
               Bring it on!
             </button>
+          </Col>
+        </Row>
+        <Row className='content-block'>
+          <Col
+            xs={12}
+            sm={12}
+            md={{ span: 8, offset: 2 }}
+            lg={{ span: 8, offset: 2 }}>
+            <QuestionAnswers />;
           </Col>
         </Row>
       </Container>
