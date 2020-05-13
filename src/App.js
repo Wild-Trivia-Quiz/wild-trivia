6fcdeb5be1003740b9ef44a845d5f396ae67c0ad
Filipa Marta <filipa.marta@gmail.com>
2020-05-13 12:41:29 +0100

    Revert "Game 1"

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Homepage from './components/Homepage';
-import About from './components/About';
-import Game from './components/Game';
-import HomepageContextProvider from './contexts/HomepageContext';
-import QuizAPIContextProvider from './contexts/QuizAPIContext';
-import GameContextProvider from './contexts/GameContext';
+import logo from './logo.svg';
 import './App.scss';
-import MainPage from './components/MainPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <HomepageContextProvider>
-          <QuizAPIContextProvider>
-            <GameContextProvider>
-              <Switch>
-                <Route exact path='/' component={MainPage} />
-                <Route path='/game' component={Game} />
-                <Route path='/about' component={About} />
-              </Switch>
-            </GameContextProvider>
-          </QuizAPIContextProvider>
-        </HomepageContextProvider>
+    <div className='App'>
+      <div className='spinner-border' role='status'>
+        <span className='sr-only'>Loading...</span>
       </div>
-    </Router>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
