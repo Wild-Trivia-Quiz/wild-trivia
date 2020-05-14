0d4c88f4aa9abc0fff68efa4983d3d678d8ede48
Filipa Marta <filipa.marta@gmail.com>
2020-05-14 21:46:04 +0100

    game styles

--- src/components/GameResult.js
+++ src/components/GameResult.js
@@ -1,21 +1,29 @@
-import React, { useContext } from 'react';
-import './GameResult.scss';
-import { QuizAPIContext } from '../contexts/QuizAPIContext';
-import Round from './Round';
-import { GameContext } from '../contexts/GameContext';
+import React, { useContext } from "react";
+import Round from "./Round";
+import { QuizAPIContext } from "../contexts/QuizAPIContext";
+import { GameContext } from "../contexts/GameContext";
+import logo from '../img/logo-white.png';
+import "./GameResult.scss";
 
 const GameResult = () => {
   const { quizQuestionsAndAnswers } = useContext(QuizAPIContext);
-  const { correctAnswersCounter } = useContext(GameContext);
+  const { round, score, correctAnswersCounter } = useContext(GameContext);
 
   return (
-    <div className='game-result'>
-      <p>
-        Congratulations, you have reached the end with {correctAnswersCounter}
-        {''}
-        correct answers. Here are all the others:
-      </p>
-      <section className='final-results'>
+    <div className="game-result">
+      <div className="message">
+        <h4>
+          Congratulations, you have reached the end with:<br /><span>{correctAnswersCounter}{" "}
+          correct answers</span>
+        </h4>
+        <img src={logo} alt='logo' className='logo' />
+        <p>Check your Trivia Game with all the questions and answers bellow.</p>
+      </div>
+      <ul className="board-results">
+        <li>Rounds: {round}/10</li>
+        <li>Score: {score}/100</li>
+      </ul>
+      <section className="final-results">
         {quizQuestionsAndAnswers.map((quizRound) => (
           <Round key={quizRound.id} quizRound={{ ...quizRound }} />
         ))}
--- src/components/QuestionAnswers.js
+++ src/components/QuestionAnswers.js
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect } from 'react';
-import { QuizAPIContext } from '../contexts/QuizAPIContext';
-import { GameContext } from '../contexts/GameContext';
-import GameResult from './GameResult';
-import './QuestionAnswers.scss';
-import Round from './Round';
+import React, { useContext, useEffect } from "react";
+import { QuizAPIContext } from "../contexts/QuizAPIContext";
+import { GameContext } from "../contexts/GameContext";
+import GameResult from "./GameResult";
+import Round from "./Round";
+import "./QuestionAnswers.scss";
 
 const QuestionAnswers = () => {
   const { quizQuestionsAndAnswers } = useContext(QuizAPIContext);
-  const { round, isGameFinished } = useContext(GameContext);
+  const { score, round, isGameFinished } = useContext(GameContext);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -18,12 +18,18 @@ const QuestionAnswers = () => {
       {isGameFinished ? (
         <GameResult />
       ) : (
-        quizQuestionsAndAnswers.map(
-          (quizRound) =>
-            quizRound.id === round && (
-              <Round key={quizRound.id} quizRound={{ ...quizRound }} />
-            )
-        )
+        <div>
+          <ul className="board-results">
+            <li>Rounds: {round}/10</li>
+            <li>Score: {score}/100</li>
+          </ul>
+          {quizQuestionsAndAnswers.map(
+            (quizRound) =>
+              quizRound.id === round && (
+                <Round key={quizRound.id} quizRound={{ ...quizRound }} />
+              )
+          )}
+        </div>
       )}
     </div>
   );
